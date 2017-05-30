62eb6e674724152fc2e8624389c6862b0bb142b0
Naveen Koka <koka.naveen@gmail.com>
2017-05-29 21:57:55 -0700

    initial commit - work in progress

---
+++ src/main/java/com/salesforce/login/salesforceoauth/SalesforceOauthApplication.java
@@ -0,0 +1,12 @@
+package com.salesforce.login.salesforceoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceOauthApplication.class, args);
+	}
+}
